b1af4e1d7c43757567a7652483d03b343d711910
Odiagbe Samson <odiagbesamsonosaro@gmail.com>
2019-07-04 15:53:35 +0000

    it has to work now

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ app.use("/auth", require("./routes/authRoutes"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 }
 
-app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}`);
 });
