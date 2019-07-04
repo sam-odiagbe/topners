d2b3ac285d150e8016968b159332a701bbfc7b99
Odiagbe Samson <odiagbesamsonosaro@gmail.com>
2019-07-04 15:57:18 +0000

    work now abeg

---
+++ client/build/precache-manifest.7f6a7f8e34f08150270977a4611ee40b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7403386b88354f9ce6ece22ce144f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "568be2055a11b458942b",
+    "url": "/static/css/main.e5597c30.chunk.css"
+  },
+  {
+    "revision": "cffd120987e8cb9fcd9f",
+    "url": "/static/js/2.921bc419.chunk.js"
+  },
+  {
+    "revision": "568be2055a11b458942b",
+    "url": "/static/js/main.ad730e21.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
