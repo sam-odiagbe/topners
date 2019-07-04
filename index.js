4ee361895848973cafb59d0e879a2f842e5d7adc
Odiagbe Samson <odiagbesamsonosaro@gmail.com>
2019-07-04 15:49:06 +0000

    was having error with my path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require("path");
 const connection = require("./database/connect");
 
 app.use(express.urlencoded({ extended: true }));
