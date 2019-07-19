5cb7c29f9edcccf1e97fdd1e8b9f8dd8fd2f2ae7
Odiagbe Samson <odiagbesamsonosaro@gmail.com>
2019-07-19 16:55:20 +0000

    pushed

--- database/connect.js
+++ database/connect.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const db = mongoose.connection;
 
-//sammyodiagbe:DjAckGFb7P7zuar@ds347367.mlab.com:47367/heroku_86xxvb3j
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/topten", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-  })
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb://sammyodiagbe:DjAckGFb7P7zuar@ds347367.mlab.com:47367/heroku_86xxvb3j",
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    }
+  )
   .then(() => {
     console.log("connected");
   })
