5985c876554b06950764a92ad784441803abd033
Odiagbe Samson <odiagbesamsonosaro@gmail.com>
2019-07-22 10:11:47 +0000

    made some awesome changes

--- index.js
+++ index.js
@@ -31,6 +31,29 @@ app.use("/auth", require("./routes/authRoutes"));
 app.use(require("./routes/gameRoutes"));
 app.use(require("./routes/adminRoutes"));
 
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", [
+    "https://topner.herokuapp.com",
+    "https://poseidonzeus.herokuapp.com"
+  ]);
+
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
